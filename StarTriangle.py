75332a181f67016d359c2def20b48047aa606c5b
yliwang <1220264221@qq.com>
2021-10-08 15:54:53 +0800

    Update StarTriangle.py

--- StarTriangle.py
+++ StarTriangle.py
@@ -1,13 +1,7 @@
 # python function printing a star triangle
 
 def OutputStarTriangle( n ):
-    for i in range( 1,n+1 ):
-        for j in range(1,n-i+1):
-            print(' ', end='')            
-        for j in range(1,2*i):
-            print( '*', end='' )
-        else:
-            print('')
+    
 
 
 
