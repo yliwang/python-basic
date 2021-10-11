4cba949af2fa23b381a7477982af80374dd591cb
yliwang <1220264221@qq.com>
2021-10-11 10:21:51 +0800

    Update LeapYear.py

--- LeapYear.py
+++ LeapYear.py
@@ -1,14 +1,5 @@
 def IsLeapYear( y ):
-    if y % 4 == 0:
-        if y % 100 == 0:
-            if y % 400 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
+   
 
 
 
