aaa870a84aef8d58d40f4eff24439eee74e218dc
yliwang <1220264221@qq.com>
2021-10-11 08:23:25 +0800

    Add files via upload

---
+++ MergeSorted.py
@@ -0,0 +1,14 @@
+def MergeSorted( l1, l2 ):
+    
+
+
+
+
+
+if __name__=='__main__':
+    l1 = [ 1, 22, 333, 4444, 55555]
+     
+    l2 = [ 3, 44, 111, 222, 666]
+
+    l = MergeSorted( l1, l2 )
+    print( l )
