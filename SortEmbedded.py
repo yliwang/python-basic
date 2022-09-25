76b9fcea15b2b51211a4789d17e95425410bd662
yliwang <1220264221@qq.com>
2022-09-26 07:58:17 +0800

    Create SortEmbedded.py

---
+++ SortEmbedded.py
@@ -0,0 +1,11 @@
+def CompareItems( item ):
+  
+  
+def SortEmbedded( l ):
+    
+
+
+if __name__=='__main__':
+    r = SortEmbedded( [1, [13, 12], [6,7,8,9]] )
+    print( r )
+    assert r == [1, [6,7,8,9], [12, 13]]
